ff11182b1c36fa4baa03b765e409831bcd0cf470
Manish Kushwaha <manishkushwaha4669@gmail.com>
2025-03-03 12:37:27 +0530

    more facility added

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import MyApp
 
 # Create your views here.
+def Home(request):
+    ItsApp = MyApp.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'ItsApp': ItsApp,
+    }
+
+    return HttpResponse(template.render(context, request))
